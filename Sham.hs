f583241c41bce4421f7aa7651022b7b66f3222ea
Will Faught <will.faught@gmail.com>
2009-08-19 01:08:03 -0700

    Split types into SType and DType. Started thinking about how to do type relations as functions. The problem is that I can't do it that way if I'm going to have a single TyDef but separate H/M expressions. I feel that there must be a way to abstract over all the languages, have a single expression data type, that I can parameterize for any language somehow. Need to think more about this.

--- Sham.hs
+++ Sham.hs
@@ -1,36 +1,40 @@
 module Sham where
 
-newtype EVar = EVar String deriving (Show, Eq)
+type EVar = String
 
-newtype TVar = TVar String deriving (Show, Eq)
+type TVar = String
 
-data TyField = TyField { tyfieldName :: String, tyfieldType :: Type } deriving (Eq, Show)
+--type ExpType = HExp -> Type
+
+data TyField = TyField { tyfieldName :: String, tyfieldType :: SType } deriving (Eq, Show)
 
 data TyCon = TyCon { tyconName :: String, tyconFields :: [TyField] } deriving (Eq, Show)
 
 data TyDef = TyDef { tydefName :: String, tydefTypes :: [TVar], tydefCons :: [TyCon] } deriving (Eq, Show)
 
-data Type = Lump
+data DType = DType
+
+data SType = Lump
   | Nat
   | TyVar TVar
-  | Label Type
-  | Fun Type Type
-  | Forall TVar Type
-  | Ext TyDef
+  | Label SType
+  | Fun SType SType
+  | Forall TVar SType
+  | Ext String [SType]
   deriving (Eq, Show)
 
 data HExp = HVar EVar
   | HApp HExp HExp
   | HFix HExp
-  | HTyApp HExp Type
+  | HTyApp HExp SType
   | HOp String HExp
   | HAdd HExp HExp
   | HSub HExp HExp
   | HIf0 HExp HExp HExp
-  | HWrong Type String
-  | HM Type MExp
-  | HS Type SExp
-  | HFunAbs EVar Type HExp
+  | HWrong SType String
+  | HM SType MExp
+  | HS SType SExp
+  | HFunAbs EVar SType HExp
   | HTyAbs TVar HExp
   | HNum Integer
   deriving (Eq, Show)
@@ -41,13 +45,16 @@ data MExp = MNum Integer
 data SExp = SNum Integer
   deriving (Eq, Show)
 
-unlabel :: Type -> Type
+--hExpType :: HContext -> HExpType
+--hExpType c (HVar x) = 
+
+unlabel :: SType -> SType
 unlabel (Label t) = t
 unlabel (Fun x y) = Fun (unlabel x) (unlabel y)
 unlabel (Forall x y) = Forall x (unlabel y)
 unlabel x = x
 
-substType :: Type -> Type -> Type -> Type
+substType :: SType -> SType -> SType -> SType
 substType _ _ Lump = Lump
 substType _ _ Nat = Nat
 substType new (TyVar x) (TyVar y) | x == y = new
@@ -55,3 +62,4 @@ substType _ _ x @ (Label _) = x
 substType new old (Fun x y) = Fun (substType new old x) (substType new old y)
 substType new old @ (TyVar x) forall @ (Forall y z) | x /= y = Forall y (substType new old z)
                                                     | otherwise = forall
+--don't forget about Ext!
