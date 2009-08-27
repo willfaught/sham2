8ce17ac70df90f556f01c32cba537d3f5c6fb6d2
Will Faught <will.faught@gmail.com>
2009-08-27 00:18:17 -0700

    Added okType to check for type well-formedness. Added a few more hType cases.

--- Sham.hs
+++ Sham.hs
@@ -22,12 +22,35 @@ substType new old @ (TyVar x) forall @ (Forall y z) | x /= y = Forall y (substTy
                                                     | otherwise = forall
 --don't forget about Ext!
 
-expect :: SType -> Maybe SType -> Maybe SType
-expect e Nothing = Nothing
-expect e (Just a) = if e == a then Just a else Nothing
+okType :: SType -> Bool
+okType t = go sempty t
+  where go c Lump = True
+        go c Nat = True
+        go c (TyVar x) = sbound x c
+        go c (Label x) = go c x
+        go c (Fun x y) = go c x && go c y
+        go c (Forall x y) = go (stbind x c) y
+        go c (Ext _ x) = and (map (go c) x)
 
 hType :: SContext -> HExp -> Maybe SType
-hType c 
 hType c (HAdd x y) = do xt <- hType c x
                         yt <- hType c y
                         if xt == Nat && yt == Nat then return Nat else Nothing
+hType c (HApp x y) = do r <- hType c x
+                        d <- hType c y
+                        case r of
+                          Fun p b -> if p == d then Just b else Nothing
+                          _ -> Nothing
+hType c (HFix x) = do t <- hType c x
+                      case t of
+                        Fun p b -> Just p
+                        _ -> Nothing
+hType c (HFunAbs v p e) = if okType p
+                          then do b <- hType (sebind v p c) e
+                                  return (Fun p b)
+                          else Nothing
+hType c (HIf0 g t f) = do gt <- hType c g
+                          tt <- hType c t
+                          ft <- hType c f
+                          if gt == Nat && tt == ft then Just tt else Nothing
+hType _ (HNum _) = Just Nat
