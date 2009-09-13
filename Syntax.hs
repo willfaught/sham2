f253e41fe1bd3f9c3a3d91b309ad78dc6bb224c2
Will Faught <will.faught@gmail.com>
2009-09-12 18:34:24 -0700

    Added query-by-name functions for TyDefs in Syntax.

--- Syntax.hs
+++ Syntax.hs
@@ -1,5 +1,8 @@
 module Syntax where
 
+import Data.List (find)
+import Data.Maybe (fromJust, isJust)
+
 type EVar = String
 
 type TVar = String
@@ -8,8 +11,10 @@ data DType =
   DType
   deriving (Eq, Show)
 
+type Name = String
+
 data SType =
-  Ext String [SType]
+  Ext Name [SType]
   | Forall TVar SType
   | Fun SType SType
   | Label SType
@@ -24,22 +29,47 @@ type FieldTyRule t = t -> t
 
 data TyField =
   TyField {
-    tyfieldName :: Maybe String,
+    tyfieldName :: Maybe Name,
     tyfieldDTyRule :: FieldTyRule DType,
     tyfieldSTyRule :: FieldTyRule SType }
 
 data TyCon =
   TyCon {
-    tyconName :: String,
+    tyconName :: Name,
     tyconFields :: [TyField],
     tyconDTyRule :: ConTyRule DType,
     tyconSTyRule :: ConTyRule SType }
 
 data TyDef =
   TyDef {
-    tydefName :: String,
-    tydefTypes :: Int,
+    tydefName :: Name,
+    tydefArity :: Int,
     tydefCons :: [TyCon] }
+
+type TyDefs = [TyDef]
+
+tydef :: Name -> TyDefs -> Maybe TyDef
+tydef n = find $ (== n) . tydefName
+
+tycon :: Name -> TyDefs -> Maybe TyCon
+tycon n = fromJust . find isJust . map ((find $ (== n) . tyconName) . tydefCons)
+
+tyfield :: Name -> TyDefs -> Maybe TyField
+tyfield n1 = fromJust . find isJust . concat . map def where
+  def = map con . tydefCons 
+  con = find match . tyconFields
+  match (TyField (Just n2) _ _) = n1 == n2
+  match (TyField Nothing _ _) = False
+
+condef :: Name -> TyDefs -> Maybe TyDef
+condef n = find $ any ((==) n . tyconName) . tydefCons
+
+fieldcon :: Name -> TyDefs -> Maybe TyCon
+fieldcon n1 = fromJust . find isJust . map def where
+  def = find con . tydefCons
+  con = any field . tyconFields
+  field (TyField (Just n2) _ _) = n1 == n2
+  field (TyField Nothing _ _) = False
 
 data Field e =
   FieldExp e
@@ -48,11 +78,11 @@ data Field e =
 
 data HExp =
   HAdd HExp HExp
-  | HCon String [Field HExp]
+  | HCon Name [Field HExp]
   | HFix HExp
   | HFunAbs EVar SType HExp
   | HFunApp HExp HExp
-  | HField String HExp
+  | HField Name HExp
   | HIf0 HExp HExp HExp
   | HM SType MExp
   | HNum Integer
