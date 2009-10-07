38411b80f7860c8e4422870fbcc0c9ace866027d
Will Faught <will.faught@gmail.com>
2009-10-06 23:56:26 -0700

    Created TypeParser.y and TypeParser.hs with Happy.

--- TypeParser.hs
+++ TypeParser.hs
@@ -1,70 +1,396 @@
-module TypeParser (stype, tvar) where
+{-# OPTIONS_GHC -fno-warn-overlapping-patterns #-}
+module TypeParser2 (parseT) where
 
+import Data.Char
+import Data.List (isPrefixOf)
+import Data.Maybe (fromJust, isJust)
 import Syntax
-import Text.ParserCombinators.Parsec hiding (label)
 
-stype :: Parser SType
-stype = chainl1 stype' (fun <?> "function abstraction")
+parseT :: String -> SType
+parseT = stype . lexer
 
-stype' = do
-  t <- stype''
-  let labels = do n <- many (label <?> "label") ; return $ foldl Label t n
-  option t labels
+-- parser produced by Happy Version 1.18.4
 
-stype'' :: Parser SType
-stype'' = (forall <?> "type abstraction")
-  <|> (lump <?> "lump")
-  <|> (nat <?> "number")
-  <|> (parens <?> "parentheses")
-  <|> (tyvar <?> "type variable")
+data HappyAbsSyn t4
+	= HappyTerminal (Token)
+	| HappyErrorToken Int
+	| HappyAbsSyn4 t4
 
-forall :: Parser SType
-forall = do
-  char 'A'
-  spaces
-  v <- tvar
-  spaces
-  char '.'
-  spaces
-  b <- stype
-  return $ Forall v b
+action_0 (5) = happyShift action_2
+action_0 (6) = happyShift action_4
+action_0 (7) = happyShift action_5
+action_0 (11) = happyShift action_6
+action_0 (4) = happyGoto action_3
+action_0 _ = happyFail
 
-fun :: Parser (SType -> SType -> SType)
-fun = do
-  spaces
-  string "->"
-  spaces
-  return Fun
+action_1 (5) = happyShift action_2
+action_1 _ = happyFail
 
-label :: Parser Int
-label = do
-  spaces
-  char '^'
-  spaces
-  n <- many1 digit
-  return $ read n
+action_2 (11) = happyShift action_9
+action_2 _ = happyFail
 
-lump :: Parser SType
-lump = do
-  char 'L'
-  return Lump
+action_3 (9) = happyShift action_7
+action_3 (10) = happyShift action_8
+action_3 (13) = happyAccept
+action_3 _ = happyFail
 
-nat :: Parser SType
-nat = do
-  char 'N'
-  return Nat
+action_4 _ = happyReduce_2
 
-parens :: Parser SType
-parens = between (char '(') (char ')') stype
+action_5 _ = happyReduce_3
 
-tvar :: Parser TVar
-tvar = do
-  c <- lower
-  cs <- many (lower <|> digit)
-  ps <- many (char '\'')
-  return $ c : cs ++ ps
+action_6 _ = happyReduce_4
 
-tyvar :: Parser SType
-tyvar = do
-  v <- tvar
-  return $ TyVar v
+action_7 (12) = happyShift action_12
+action_7 _ = happyFail
+
+action_8 (5) = happyShift action_2
+action_8 (6) = happyShift action_4
+action_8 (7) = happyShift action_5
+action_8 (11) = happyShift action_6
+action_8 (4) = happyGoto action_11
+action_8 _ = happyFail
+
+action_9 (8) = happyShift action_10
+action_9 _ = happyFail
+
+action_10 (5) = happyShift action_2
+action_10 (6) = happyShift action_4
+action_10 (7) = happyShift action_5
+action_10 (11) = happyShift action_6
+action_10 (4) = happyGoto action_13
+action_10 _ = happyFail
+
+action_11 (9) = happyShift action_7
+action_11 (10) = happyShift action_8
+action_11 _ = happyReduce_6
+
+action_12 _ = happyReduce_5
+
+action_13 (9) = happyShift action_7
+action_13 (10) = happyShift action_8
+action_13 _ = happyReduce_1
+
+happyReduce_1 = happyReduce 4 4 happyReduction_1
+happyReduction_1 ((HappyAbsSyn4  happy_var_4) `HappyStk`
+	_ `HappyStk`
+	(HappyTerminal (TokenVariable happy_var_2)) `HappyStk`
+	_ `HappyStk`
+	happyRest)
+	 = HappyAbsSyn4
+		 (Forall happy_var_2 happy_var_4
+	) `HappyStk` happyRest
+
+happyReduce_2 = happySpecReduce_1  4 happyReduction_2
+happyReduction_2 _
+	 =  HappyAbsSyn4
+		 (Lump
+	)
+
+happyReduce_3 = happySpecReduce_1  4 happyReduction_3
+happyReduction_3 _
+	 =  HappyAbsSyn4
+		 (Nat
+	)
+
+happyReduce_4 = happySpecReduce_1  4 happyReduction_4
+happyReduction_4 (HappyTerminal (TokenVariable happy_var_1))
+	 =  HappyAbsSyn4
+		 (TyVar happy_var_1
+	)
+happyReduction_4 _  = notHappyAtAll 
+
+happyReduce_5 = happySpecReduce_3  4 happyReduction_5
+happyReduction_5 (HappyTerminal (TokenLabel happy_var_3))
+	_
+	(HappyAbsSyn4  happy_var_1)
+	 =  HappyAbsSyn4
+		 (Label happy_var_1 happy_var_3
+	)
+happyReduction_5 _ _ _  = notHappyAtAll 
+
+happyReduce_6 = happySpecReduce_3  4 happyReduction_6
+happyReduction_6 (HappyAbsSyn4  happy_var_3)
+	_
+	(HappyAbsSyn4  happy_var_1)
+	 =  HappyAbsSyn4
+		 (Fun happy_var_1 happy_var_3
+	)
+happyReduction_6 _ _ _  = notHappyAtAll 
+
+happyNewToken action sts stk [] =
+	action 13 13 notHappyAtAll (HappyState action) sts stk []
+
+happyNewToken action sts stk (tk:tks) =
+	let cont i = action i i tk (HappyState action) sts stk tks in
+	case tk of {
+	TokenForall -> cont 5;
+	TokenLump -> cont 6;
+	TokenNumber -> cont 7;
+	TokenPeriod -> cont 8;
+	TokenCaret -> cont 9;
+	TokenArrow -> cont 10;
+	TokenVariable happy_dollar_dollar -> cont 11;
+	TokenLabel happy_dollar_dollar -> cont 12;
+	_ -> happyError' (tk:tks)
+	}
+
+happyError_ tk tks = happyError' (tk:tks)
+
+newtype HappyIdentity a = HappyIdentity a
+happyIdentity = HappyIdentity
+happyRunIdentity (HappyIdentity a) = a
+
+instance Monad HappyIdentity where
+    return = HappyIdentity
+    (HappyIdentity p) >>= q = q p
+
+happyThen :: () => HappyIdentity a -> (a -> HappyIdentity b) -> HappyIdentity b
+happyThen = (>>=)
+happyReturn :: () => a -> HappyIdentity a
+happyReturn = (return)
+happyThen1 m k tks = (>>=) m (\a -> k a tks)
+happyReturn1 :: () => a -> b -> HappyIdentity a
+happyReturn1 = \a tks -> (return) a
+happyError' :: () => [(Token)] -> HappyIdentity a
+happyError' = HappyIdentity . parseError
+
+stype tks = happyRunIdentity happySomeParser where
+  happySomeParser = happyThen (happyParse action_0 tks) (\x -> case x of {HappyAbsSyn4 z -> happyReturn z; _other -> notHappyAtAll })
+
+happySeq = happyDontSeq
+
+
+parseError :: [Token] -> a
+parseError _ = error "Parse error"
+
+data Token = TokenArrow
+  | TokenCaret
+  | TokenForall
+  | TokenLabel Int
+  | TokenLump
+  | TokenNumber
+  | TokenPeriod
+  | TokenVariable String
+  deriving Show
+
+lexer :: String -> [Token]
+lexer [] = []
+lexer cs | "->" `isPrefixOf` cs = TokenArrow : lexer (drop 2 cs)
+lexer (c : cs) | isJust $ lookup c tokens = fromJust (lookup c tokens) : lexer cs
+  where tokens = [('A', TokenForall), ('L', TokenLump), ('N', TokenNumber), ('.', TokenPeriod), ('^', TokenCaret)]
+lexer s @ (c : cs)
+  | isSpace c = lexer cs
+  | isAlpha c = lexAlpha s
+  | isDigit c = lexDigit s
+
+lexAlpha (c : cs) = TokenVariable (c : both ++ primes) : lexer primes'
+  where
+    (both, both') = span (\x -> isAlpha x || isDigit x) cs
+    (primes, primes') = span ((==) '\'') both'
+
+lexDigit cs = (TokenLabel (read num)) : lexer rest
+  where (num, rest) = span isDigit cs
+{-# LINE 1 "templates/GenericTemplate.hs" #-}
+{-# LINE 1 "templates/GenericTemplate.hs" #-}
+{-# LINE 1 "<built-in>" #-}
+{-# LINE 1 "<command line>" #-}
+{-# LINE 1 "templates/GenericTemplate.hs" #-}
+-- Id: GenericTemplate.hs,v 1.26 2005/01/14 14:47:22 simonmar Exp 
+
+{-# LINE 28 "templates/GenericTemplate.hs" #-}
+
+
+
+
+
+
+
+
+{-# LINE 49 "templates/GenericTemplate.hs" #-}
+
+{-# LINE 59 "templates/GenericTemplate.hs" #-}
+
+{-# LINE 68 "templates/GenericTemplate.hs" #-}
+
+infixr 9 `HappyStk`
+data HappyStk a = HappyStk a (HappyStk a)
+
+-----------------------------------------------------------------------------
+-- starting the parse
+
+happyParse start_state = happyNewToken start_state notHappyAtAll notHappyAtAll
+
+-----------------------------------------------------------------------------
+-- Accepting the parse
+
+-- If the current token is (1), it means we've just accepted a partial
+-- parse (a %partial parser).  We must ignore the saved token on the top of
+-- the stack in this case.
+happyAccept (1) tk st sts (_ `HappyStk` ans `HappyStk` _) =
+	happyReturn1 ans
+happyAccept j tk st sts (HappyStk ans _) = 
+	 (happyReturn1 ans)
+
+-----------------------------------------------------------------------------
+-- Arrays only: do the next action
+
+{-# LINE 155 "templates/GenericTemplate.hs" #-}
+
+-----------------------------------------------------------------------------
+-- HappyState data type (not arrays)
+
+
+
+newtype HappyState b c = HappyState
+        (Int ->                    -- token number
+         Int ->                    -- token number (yes, again)
+         b ->                           -- token semantic value
+         HappyState b c ->              -- current state
+         [HappyState b c] ->            -- state stack
+         c)
+
+
+
+-----------------------------------------------------------------------------
+-- Shifting a token
+
+happyShift new_state (1) tk st sts stk@(x `HappyStk` _) =
+     let i = (case x of { HappyErrorToken (i) -> i }) in
+--     trace "shifting the error token" $
+     new_state i i tk (HappyState (new_state)) ((st):(sts)) (stk)
+
+happyShift new_state i tk st sts stk =
+     happyNewToken new_state ((st):(sts)) ((HappyTerminal (tk))`HappyStk`stk)
+
+-- happyReduce is specialised for the common cases.
+
+happySpecReduce_0 i fn (1) tk st sts stk
+     = happyFail (1) tk st sts stk
+happySpecReduce_0 nt fn j tk st@((HappyState (action))) sts stk
+     = action nt j tk st ((st):(sts)) (fn `HappyStk` stk)
+
+happySpecReduce_1 i fn (1) tk st sts stk
+     = happyFail (1) tk st sts stk
+happySpecReduce_1 nt fn j tk _ sts@(((st@(HappyState (action))):(_))) (v1`HappyStk`stk')
+     = let r = fn v1 in
+       happySeq r (action nt j tk st sts (r `HappyStk` stk'))
+
+happySpecReduce_2 i fn (1) tk st sts stk
+     = happyFail (1) tk st sts stk
+happySpecReduce_2 nt fn j tk _ ((_):(sts@(((st@(HappyState (action))):(_))))) (v1`HappyStk`v2`HappyStk`stk')
+     = let r = fn v1 v2 in
+       happySeq r (action nt j tk st sts (r `HappyStk` stk'))
+
+happySpecReduce_3 i fn (1) tk st sts stk
+     = happyFail (1) tk st sts stk
+happySpecReduce_3 nt fn j tk _ ((_):(((_):(sts@(((st@(HappyState (action))):(_))))))) (v1`HappyStk`v2`HappyStk`v3`HappyStk`stk')
+     = let r = fn v1 v2 v3 in
+       happySeq r (action nt j tk st sts (r `HappyStk` stk'))
+
+happyReduce k i fn (1) tk st sts stk
+     = happyFail (1) tk st sts stk
+happyReduce k nt fn j tk st sts stk
+     = case happyDrop (k - ((1) :: Int)) sts of
+	 sts1@(((st1@(HappyState (action))):(_))) ->
+        	let r = fn stk in  -- it doesn't hurt to always seq here...
+       		happyDoSeq r (action nt j tk st1 sts1 r)
+
+happyMonadReduce k nt fn (1) tk st sts stk
+     = happyFail (1) tk st sts stk
+happyMonadReduce k nt fn j tk st sts stk =
+        happyThen1 (fn stk tk) (\r -> action nt j tk st1 sts1 (r `HappyStk` drop_stk))
+       where sts1@(((st1@(HappyState (action))):(_))) = happyDrop k ((st):(sts))
+             drop_stk = happyDropStk k stk
+
+happyMonad2Reduce k nt fn (1) tk st sts stk
+     = happyFail (1) tk st sts stk
+happyMonad2Reduce k nt fn j tk st sts stk =
+       happyThen1 (fn stk tk) (\r -> happyNewToken new_state sts1 (r `HappyStk` drop_stk))
+       where sts1@(((st1@(HappyState (action))):(_))) = happyDrop k ((st):(sts))
+             drop_stk = happyDropStk k stk
+
+
+
+
+
+             new_state = action
+
+
+happyDrop (0) l = l
+happyDrop n ((_):(t)) = happyDrop (n - ((1) :: Int)) t
+
+happyDropStk (0) l = l
+happyDropStk n (x `HappyStk` xs) = happyDropStk (n - ((1)::Int)) xs
+
+-----------------------------------------------------------------------------
+-- Moving to a new state after a reduction
+
+{-# LINE 253 "templates/GenericTemplate.hs" #-}
+happyGoto action j tk st = action j j tk (HappyState action)
+
+
+-----------------------------------------------------------------------------
+-- Error recovery ((1) is the error token)
+
+-- parse error if we are in recovery and we fail again
+happyFail  (1) tk old_st _ stk =
+--	trace "failing" $ 
+    	happyError_ tk
+
+{-  We don't need state discarding for our restricted implementation of
+    "error".  In fact, it can cause some bogus parses, so I've disabled it
+    for now --SDM
+
+-- discard a state
+happyFail  (1) tk old_st (((HappyState (action))):(sts)) 
+						(saved_tok `HappyStk` _ `HappyStk` stk) =
+--	trace ("discarding state, depth " ++ show (length stk))  $
+	action (1) (1) tk (HappyState (action)) sts ((saved_tok`HappyStk`stk))
+-}
+
+-- Enter error recovery: generate an error token,
+--                       save the old token and carry on.
+happyFail  i tk (HappyState (action)) sts stk =
+--      trace "entering error recovery" $
+	action (1) (1) tk (HappyState (action)) sts ( (HappyErrorToken (i)) `HappyStk` stk)
+
+-- Internal happy errors:
+
+notHappyAtAll = error "Internal Happy error\n"
+
+-----------------------------------------------------------------------------
+-- Hack to get the typechecker to accept our action functions
+
+
+
+
+
+
+
+-----------------------------------------------------------------------------
+-- Seq-ing.  If the --strict flag is given, then Happy emits 
+--	happySeq = happyDoSeq
+-- otherwise it emits
+-- 	happySeq = happyDontSeq
+
+happyDoSeq, happyDontSeq :: a -> b -> b
+happyDoSeq   a b = a `seq` b
+happyDontSeq a b = b
+
+-----------------------------------------------------------------------------
+-- Don't inline any functions from the template.  GHC has a nasty habit
+-- of deciding to inline happyGoto everywhere, which increases the size of
+-- the generated parser quite a bit.
+
+{-# LINE 317 "templates/GenericTemplate.hs" #-}
+{-# NOINLINE happyShift #-}
+{-# NOINLINE happySpecReduce_0 #-}
+{-# NOINLINE happySpecReduce_1 #-}
+{-# NOINLINE happySpecReduce_2 #-}
+{-# NOINLINE happySpecReduce_3 #-}
+{-# NOINLINE happyReduce #-}
+{-# NOINLINE happyMonadReduce #-}
+{-# NOINLINE happyGoto #-}
+{-# NOINLINE happyFail #-}
+
+-- end of Happy Template.
