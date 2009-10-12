350afac1136dc4b67a43d045c73e1f65c55d2f3e
Will Faught <will.faught@gmail.com>
2009-10-11 18:53:51 -0700

    Main.hs: Added evalH.

--- Main.hs
+++ Main.hs
@@ -1,1 +1,13 @@
-module Sham where
+module Main (evalH) where
+
+import Check
+import Context
+import Data.Maybe (isJust)
+import HaskellParser
+import Reduce
+import Syntax
+
+evalH :: String -> Maybe HExp
+evalH s = case parseH s of
+  Left x -> Nothing
+  Right x -> if isJust $ checkH empty x then Just $ reduceFullH x else Nothing
