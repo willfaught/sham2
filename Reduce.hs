32b001bf0d64ee179aa11ecfe0a42ce8371ef8b1
Will Faught <will.faught@gmail.com>
2009-10-11 17:36:07 -0700

    Reduce.hs: Added reduceM and reduceFullM.

--- Reduce.hs
+++ Reduce.hs
@@ -1,10 +1,10 @@
-module Reduce (reduceH) where
+module Reduce (reduceH, reduceM, reduceFullH, reduceFullM) where
 
 import Subst
 import Syntax
 
 unlabel :: SType -> SType
-unlabel (Label t) = t
+unlabel (Label t _) = t
 unlabel (Fun x y) = Fun (unlabel x) (unlabel y)
 unlabel (Forall x y) = Forall x (unlabel y)
 unlabel x = x
@@ -16,9 +16,21 @@ valueH (HNum _) = True
 valueH (HTyAbs _ _) = True
 valueH _ = False
 
+valueM :: MExp -> Bool
+--valueM (MCon _ _) = True
+valueM (MFunAbs _ _ _) = True
+valueM (MNum _) = True
+valueM (MTyAbs _ _) = True
+valueM _ = False
+
 reduceFullH :: HExp -> HExp
 reduceFullH x = case reduceH x of
   Just x' -> reduceFullH x'
+  Nothing -> x
+
+reduceFullM :: MExp -> MExp
+reduceFullM x = case reduceM x of
+  Just x' -> reduceFullM x'
   Nothing -> x
 
 reduceH :: HExp -> Maybe HExp
@@ -56,3 +68,39 @@ reduceH (HTyApp x y) = do
   return $ HTyApp x' y
 reduceH (HWrong _ s) = error s
 reduceH _ = Nothing
+
+reduceM :: MExp -> Maybe MExp
+reduceM (MAdd (MNum x) (MNum y)) = return . MNum $ x + y
+reduceM (MAdd x y) | not $ valueM x = do
+  x' <- reduceM x
+  return $ MAdd x' y
+reduceM (MAdd x y) = do
+  y' <- reduceM y
+  return $ MAdd x y'
+reduceM f @ (MFix (MFunAbs v _ b)) = return $ substExpM f v b
+reduceM (MFix x) = do
+  x' <- reduceM x
+  return $ MFix x'
+reduceM (MFunApp (MFunAbs v t b) a) = return $ substExpM a v b
+reduceM (MFunApp x y) = do
+  x' <- reduceM x
+  return $ MFunApp x' y
+--reduceM (MField fn (MCon cn f)) = 
+reduceM (MIf0 (MNum 0) t _) = Just t
+reduceM (MIf0 (MNum _) _ f) = Just f
+reduceM (MIf0 x t f) = do
+  x' <- reduceM x
+  return $ MIf0 x' t f
+reduceM (MSub (MNum x) (MNum y)) = Just . MNum $ x + y
+reduceM (MSub x y) | not $ valueM x = do
+  x' <- reduceM x
+  return $ MSub x' y
+reduceM (MSub x y) = do
+  y' <- reduceM y
+  return $ MSub x y'
+reduceM (MTyApp (MTyAbs v b) t) = return $ substTyExpM t v b
+reduceM (MTyApp x y) = do
+  x' <- reduceM x
+  return $ MTyApp x' y
+reduceM (MWrong _ s) = error s
+reduceM _ = Nothing
