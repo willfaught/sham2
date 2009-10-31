2f00802d2eb0d27f9f69ed2dc4da5d8fad59e881
Will Faught <will.faught@gmail.com>
2009-10-31 14:33:17 -0700

    Check.hs: Rearranged closed.

--- Check.hs
+++ Check.hs
@@ -8,13 +8,13 @@ import Syntax
 
 closed :: SType -> Bool
 closed t = go empty t where
-  go c Lump = True
-  go c Nat = True
-  go c (TyVar x) = sbound x c
-  go c (Label x _) = go c x
-  go c (Fun x y) = go c x && go c y
-  go c (Forall x y) = go (stbind x c) y
-  --go c (Ext _ x) = and (map (go c) x)
+  go c t = case t of
+    Lump -> True
+    Nat -> True
+    TyVar x -> sbound x c
+    Label x _ -> go c x
+    Fun x y -> go c x && go c y
+    Forall x y -> go (stbind x c) y
 
 assert :: Bool -> Maybe Bool
 assert True = Just True
@@ -28,19 +28,6 @@ checkH c (HAdd x y) = do
   u <- checkH c y
   assert (t == Nat && u == Nat)
   return Nat
-{-checkH c (HCon n f1) = do -- need to unify tyvars here to determine type params for con's type
-  TyCon _ f2 _ s <- tycon n d
-  assert (length f1 == length f2)
-  let fieldType (FieldExp x) = checkH c x
-      fieldType (FieldType x) = do
-        assert (closed x)
-        return x
-  t <- sequence $ map fieldType f1
-  TyDef _ a _ <- condef n d
-  assert (a == 
-  let e @ (Ext _ a) = s t
-  assert (length a == 
-  return e-}
 checkH c (HFix x) = do
   t <- checkH c x
   case t of
@@ -58,31 +45,18 @@ checkH c (HFunApp x y) = do
       assert (p == opd)
       return b
     _ -> Nothing
-{-checkH c (HField n e) = do
-  TyField _ _ s <- tyfield n d
-  t <- checkH c e
-  return $ s t-}
 checkH c (HIf0 g t f) = do
   gt <- checkH c g
   tt <- checkH c t
   ft <- checkH c f
   assert (gt == Nat && tt == ft)
   return tt
-{-checkH c (HM t m) = do
-  assert (closed t)
-  u <- checkM d c m
-  assert (t == u)
-  return t-}
 checkH c (HM t e) = do
   assert (closed t)
   u <- checkM c e
   assert (t == u)
   return t
 checkH _ (HNum _) = Just Nat
-{-checkH c (HS t e) = do
-  assert (closed t)
-  checkS d c e
-  return t-}
 checkH c (HSub x y) = do
   t <- checkH c x
   assert (t == Nat)
@@ -95,7 +69,7 @@ checkH c (HTyAbs v e) = do
 checkH c (HTyApp e t) = do
   assert (closed t)
   Forall v u <- checkH c e
-  return $ substTyTy t v u
+  return $ substTy t v u
 checkH c (HVar v) = sbinding v c
 checkH c (HWrong t s) = do
   assert (closed t)
@@ -109,19 +83,6 @@ checkM c (MAdd x y) = do
   u <- checkM c y
   assert (t == Nat && u == Nat)
   return Nat
-{-checkM c (MCon n f1) = do -- need to unify tyvars here to determine type params for con's type
-  TyCon _ f2 _ s <- tycon n d
-  assert (length f1 == length f2)
-  let fieldType (FieldExp x) = checkM c x
-      fieldType (FieldType x) = do
-        assert (closed x)
-        return x
-  t <- sequence $ map fieldType f1
-  TyDef _ a _ <- condef n d
-  assert (a == 
-  let e @ (Ext _ a) = s t
-  assert (length a == 
-  return e-}
 checkM c (MFix x) = do
   t <- checkM c x
   case t of
@@ -139,10 +100,6 @@ checkM c (MFunApp x y) = do
       assert (p == opd)
       return b
     _ -> Nothing
-{-checkM c (MField n e) = do
-  TyField _ _ s <- tyfield n d
-  t <- checkM c e
-  return $ s t-}
 checkM c (MH t e) = do
   assert (closed t)
   u <- checkH c e
@@ -154,16 +111,7 @@ checkM c (MIf0 g t f) = do
   ft <- checkM c f
   assert (gt == Nat && tt == ft)
   return tt
-{-checkM c (MM t m) = do
-  assert (closed t)
-  u <- checkM d c m
-  assert (t == u)
-  return t-}
 checkM _ (MNum _) = Just Nat
-{-checkM c (MS t e) = do
-  assert (closed t)
-  checkS d c e
-  return t-}
 checkM c (MSub x y) = do
   t <- checkM c x
   assert (t == Nat)
@@ -176,7 +124,7 @@ checkM c (MTyAbs v e) = do
 checkM c (MTyApp e t) = do
   assert (closed t)
   Forall v u <- checkM c e
-  return $ substTyTy t v u
+  return $ substTy t v u
 checkM c (MVar v) = sbinding v c
 checkM c (MWrong t s) = do
   assert (closed t)
