26cd159e7fad44f4f6aadb8e40c714ddedbbd8c9
Will Faught <will.faught@gmail.com>
2009-11-02 23:36:27 -0800

    Check.hs: Worked on new check monad. Doesn't compile.

--- Check.hs
+++ Check.hs
@@ -6,31 +6,77 @@ import Data.Maybe (fromJust, isJust)
 import Subst
 import Syntax
 
-closed :: SType -> Bool
-closed t = go empty t where
-  go c t = case t of
-    Lump -> True
-    Nat -> True
-    TyVar x -> sbound x c
-    Label x _ -> go c x
-    Fun x y -> go c x && go c y
-    Forall x y -> go (stbind x c) y
+closed :: Check SType a
+closed t = case t of
+  Lump -> return Lump
+  Nat -> return Nat
+  TyVar x -> return $ sbound x c
+  Label x _ -> return $ closed x
+  Fun x y -> return $ Fun (closed x) && (closed y)
+  Forall x y -> return $ closed (stbind x c) y
 
 assert :: Bool -> Maybe Bool
 assert True = Just True
 assert False = Nothing
 
-data CheckError = Mismatch {
-    mismatchExp :: SType,
-    mismatchAct :: SType }
+data CheckError e t = Wrong {
+    wrongExp :: e,
+    wrongExpected :: t,
+    wrongActual :: t }
+  | Mismatch {
+    mismatchFstExp :: e,
+    mismatchFstTy :: t,
+    mismatchSndExp :: e,
+    mismatchSndTy :: t }
+  deriving (Show, Eq)
 
 class Check a b | a -> b where
-  check :: Context -> a -> Either CheckError b
+  check :: Context -> a -> Either (CheckError a b) b
+
+instance Check HExp SType where
+  check = checkH
+
+newtype Check t e = Check { runCheck :: Context -> Either (CheckError e t) t }
+
+instance Monad (Check SType) where
+  Check x >>= f = Check $ \ c -> case x c of
+    Left e -> Left e
+    Right t -> f t $ c
+  x >> y = x >>= (\ _ -> y)
+  return x = Check (\ _ -> Right x)
+
+is :: a -> SType -> Check SType a
+is e t1 = do
+  t2 <- check e
+  if t1 == t2 then return t1 else Left $ Wrong e t1 t2
+
+matches :: a -> a -> Check SType a
+matches e1 e2 = do
+  t1 <- check e1
+  t2 <- check e2
+  if t1 == t2 then return t1 else Left $ Mismatch e1 t1 e2 t2
+
+context :: 
 
 -- Haskell
 
-checkH :: Context -> HExp -> Maybe SType
+checkH :: Context -> HExp -> Either (CheckError HExp SType) SType
 checkH cxt exp = case exp of
+  HAdd x y -> do
+    x `is` Nat
+    y `is` Nat
+    return Nat
+  HFix x -> do
+    x `satisfies` (\ x -> case x of Fun _ _ -> True ; _ -> False)
+  HFunAbs v p e -> do
+    closed p
+    t <- check (sebind v p context) e
+  HIf0 x y z -> do
+    x `is` Nat
+    y `matches` z
+    return y
+
+  --
   HAdd x y -> do
     t <- checkH cxt x
     u <- checkH cxt y
