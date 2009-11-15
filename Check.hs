f31aa8a2095b875ceaea9ac4babc4ab1f1b31ae6
Will Faught <will.faught@gmail.com>
2009-11-14 16:05:42 -0800

    Check.hs: Added Either result to checkH.

--- Check.hs
+++ Check.hs
@@ -1,116 +1,112 @@
-module Check (checkH, checkM) where
+module Check (check) where
 
 import Context
-import Data.List (find)
-import Data.Maybe (fromJust, isJust)
 import Subst
 import Syntax
 
-closed :: Check SType a
-closed t = case t of
-  Lump -> return Lump
-  Nat -> return Nat
-  TyVar x -> return $ sbound x c
-  Label x _ -> return $ closed x
-  Fun x y -> return $ Fun (closed x) && (closed y)
-  Forall x y -> return $ closed (stbind x c) y
-
-assert :: Bool -> Maybe Bool
-assert True = Just True
-assert False = Nothing
-
-data CheckError e t = Wrong {
-    wrongExp :: e,
-    wrongExpected :: t,
-    wrongActual :: t }
+data SError e t = Unexpected {
+    unexExp :: e,
+    unexExpected :: t,
+    unexActual :: t }
   | Mismatch {
     mismatchFstExp :: e,
     mismatchFstTy :: t,
     mismatchSndExp :: e,
     mismatchSndTy :: t }
+  | Unequal {
+    unequalFst :: t,
+    unequalSnd :: t }
+  | Wrong {
+    wrongExp :: e,
+    wrongTy :: t,
+    wrongMsg :: Wrong }
+  | Open {
+    openTy :: t,
+    openVar :: TVar }
+  | Trace {
+    traceOuter :: e,
+    traceInner :: SError e t }
+  deriving (Show, Eq)
+
+data Wrong = ExpectingFunction
+  | ExpectFunParamResEq
+  | ParamArgMismatch
   deriving (Show, Eq)
 
 class Check a b | a -> b where
-  check :: Context -> a -> Either (CheckError a b) b
+  check :: Context -> a -> Either (SError a b) b
 
 instance Check HExp SType where
   check = checkH
 
-newtype Check t e = Check { runCheck :: Context -> Either (CheckError e t) t }
+instance Check MExp SType where
+  check = checkM
 
-instance Monad (Check SType) where
-  Check x >>= f = Check $ \ c -> case x c of
-    Left e -> Left e
-    Right t -> f t $ c
-  x >> y = x >>= (\ _ -> y)
-  return x = Check (\ _ -> Right x)
-
-is :: a -> SType -> Check SType a
-is e t1 = do
-  t2 <- check e
-  if t1 == t2 then return t1 else Left $ Wrong e t1 t2
-
-matches :: a -> a -> Check SType a
-matches e1 e2 = do
-  t1 <- check e1
-  t2 <- check e2
-  if t1 == t2 then return t1 else Left $ Mismatch e1 t1 e2 t2
-
-context :: 
+free :: Context -> SType -> Maybe TVar
+free c t = case t of
+  Lump -> Nothing
+  Nat -> Nothing
+  TyVar x -> if sbound x c then Nothing else return x
+  Label x _ -> free c x
+  Fun x y -> let
+    p = free c x
+    r = free c y in case p of
+      Nothing -> r
+      Just _ -> p
+  Forall x y -> free (stbind x c) y
 
 -- Haskell
 
-checkH :: Context -> HExp -> Either (CheckError HExp SType) SType
+checkH :: Context -> HExp -> Either (SError HExp SType) SType
 checkH cxt exp = case exp of
-  HAdd x y -> do
-    x `is` Nat
-    y `is` Nat
-    return Nat
-  HFix x -> do
-    x `satisfies` (\ x -> case x of Fun _ _ -> True ; _ -> False)
-  HFunAbs v p e -> do
-    closed p
-    t <- check (sebind v p context) e
-  HIf0 x y z -> do
-    x `is` Nat
-    y `matches` z
-    return y
-
+  HAdd x y -> let
+    t = check cxt x
+    u = check cxt y in case t of
+      Left e -> Left $ Trace exp e
+      Right Nat -> case u of
+        Left e -> Left $ Trace exp e
+        Right Nat -> Right Nat
+        Right u' -> Left $ Unexpected y Nat u'
+      Right t' -> Left $ Unexpected x Nat t'
+  HFix x -> let t = check cxt x in case t of
+    Left e -> Left $ Trace exp e
+    Right (Fun p r) | p == r -> Right p
+    Right t' @ (Fun p r) -> Left (Trace exp (Wrong x t' ExpectFunParamResEq))
+    Right t' -> Left . Trace exp $ Wrong x t' ExpectingFunction
+  HFunAbs v t b -> let
+    f = free cxt t
+    u = check (sebind v t cxt) b in case f of
+      Nothing -> case u of
+        Left e -> Left $ Trace exp e
+        Right u' -> Right $ Fun t u'
+      Just f' -> Left . Trace exp $ Open t f'
+  HFunApp x y -> let
+    t = check cxt x
+    u = check cxt y in case t of
+      Left e -> Left $ Trace exp e
+      Right (Fun p r) -> case u of
+        Left e -> Left $ Trace exp e
+        Right u' | u' == p -> Right u'
+        Right u' -> Left . Trace exp $ Wrong y u' ParamArgMismatch
+      Right t' -> Left (Trace exp (Wrong x t' ExpectingFunction))
+  HIf0 x y z -> case check cxt x of
+    Left e -> Left $ Trace exp e
+    Right Nat -> case check cxt y of
+      Left e -> Left $ Trace exp e
+      Right y' -> case check cxt z of
+        Left e -> Left $ Trace exp e
+        Right z' | z' == y' -> Right z'
+        Right z' -> Left . Trace exp $ Mismatch y y' z z'
+    Right x' -> Left . Trace exp $ Unexpected x Nat x'
+  HM t x -> case free cxt t of
+    Just t' -> Left . Trace exp $ Open t t'
+    Nothing -> case check cxt x of
+      Left e -> Left $ Trace exp e
+      Right x' | x' == t -> Right x'
+      Right x' -> Left . Trace exp $ Unequal x' t
+  
   --
-  HAdd x y -> do
-    t <- checkH cxt x
-    u <- checkH cxt y
-    assert (t == Nat && u == Nat)
-    return Nat
-  HFix x -> do
-    t <- checkH cxt x
-    case t of
-      Fun p b -> Just p
-      _ -> Nothing
-  HFunAbs v p e -> do
-    assert (closed p)
-    b <- checkH (sebind v p cxt) e
-    return (Fun p b)
-  HFunApp x y -> do
-    opr <- checkH cxt x
-    opd <- checkH cxt y
-    case opr of
-      Fun p b -> do
-        assert (p == opd)
-        return b
-      _ -> Nothing
-  HIf0 g t f -> do
-    gt <- checkH cxt g
-    tt <- checkH cxt t
-    ft <- checkH cxt f
-    assert (gt == Nat && tt == ft)
-    return tt
-  HM t e -> do
-    assert (closed t)
-    u <- checkM cxt e
-    assert (t == u)
-    return t
-  HNum _ -> Just Nat
+{-HNum _ -> Just Nat
   HSub x y -> do
     t <- checkH cxt x
     assert (t == Nat)
@@ -127,11 +123,12 @@ checkH cxt exp = case exp of
   HVar v -> sbinding v cxt
   HWrong t s -> do
     assert (closed t)
-    return t
+    return t-}
 
 -- ML
 
-checkM :: Context -> MExp -> Maybe SType
+checkM = undefined
+{-checkM :: Context -> MExp -> Maybe SType
 checkM c (MAdd x y) = do
   t <- checkM c x
   u <- checkM c y
@@ -182,7 +179,7 @@ checkM c (MTyApp e t) = do
 checkM c (MVar v) = sbinding v c
 checkM c (MWrong t s) = do
   assert (closed t)
-  return t
+  return t-}
 
 {-checkS :: [TyDef] -> Context -> SExp -> Maybe DType
 checkS _ _ = undefined-}
