26a3af4a25a4c4ccf83aa2f46e87de5efd248fd9
Will Faught <will.faught@gmail.com>
2009-11-14 16:10:06 -0800

    Check.hs: Reverted to a previous version where checkH didn't return an Either. Simpler. Need to move forward.

--- Check.hs
+++ Check.hs
@@ -1,112 +1,63 @@
-module Check (check) where
+module Check (checkH, checkM) where
 
 import Context
+import Data.List (find)
+import Data.Maybe (fromJust, isJust)
 import Subst
 import Syntax
 
-data SError e t = Unexpected {
-    unexExp :: e,
-    unexExpected :: t,
-    unexActual :: t }
-  | Mismatch {
-    mismatchFstExp :: e,
-    mismatchFstTy :: t,
-    mismatchSndExp :: e,
-    mismatchSndTy :: t }
-  | Unequal {
-    unequalFst :: t,
-    unequalSnd :: t }
-  | Wrong {
-    wrongExp :: e,
-    wrongTy :: t,
-    wrongMsg :: Wrong }
-  | Open {
-    openTy :: t,
-    openVar :: TVar }
-  | Trace {
-    traceOuter :: e,
-    traceInner :: SError e t }
-  deriving (Show, Eq)
+closed :: SType -> Bool
+closed t = go empty t where
+  go c t = case t of
+    Lump -> True
+    Nat -> True
+    TyVar x -> sbound x c
+    Label x _ -> go c x
+    Fun x y -> go c x && go c y
+    Forall x y -> go (stbind x c) y
 
-data Wrong = ExpectingFunction
-  | ExpectFunParamResEq
-  | ParamArgMismatch
-  deriving (Show, Eq)
-
-class Check a b | a -> b where
-  check :: Context -> a -> Either (SError a b) b
-
-instance Check HExp SType where
-  check = checkH
-
-instance Check MExp SType where
-  check = checkM
-
-free :: Context -> SType -> Maybe TVar
-free c t = case t of
-  Lump -> Nothing
-  Nat -> Nothing
-  TyVar x -> if sbound x c then Nothing else return x
-  Label x _ -> free c x
-  Fun x y -> let
-    p = free c x
-    r = free c y in case p of
-      Nothing -> r
-      Just _ -> p
-  Forall x y -> free (stbind x c) y
+assert :: Bool -> Maybe Bool
+assert True = Just True
+assert False = Nothing
 
 -- Haskell
 
-checkH :: Context -> HExp -> Either (SError HExp SType) SType
+checkH :: Context -> HExp -> Maybe SType
 checkH cxt exp = case exp of
-  HAdd x y -> let
-    t = check cxt x
-    u = check cxt y in case t of
-      Left e -> Left $ Trace exp e
-      Right Nat -> case u of
-        Left e -> Left $ Trace exp e
-        Right Nat -> Right Nat
-        Right u' -> Left $ Unexpected y Nat u'
-      Right t' -> Left $ Unexpected x Nat t'
-  HFix x -> let t = check cxt x in case t of
-    Left e -> Left $ Trace exp e
-    Right (Fun p r) | p == r -> Right p
-    Right t' @ (Fun p r) -> Left (Trace exp (Wrong x t' ExpectFunParamResEq))
-    Right t' -> Left . Trace exp $ Wrong x t' ExpectingFunction
-  HFunAbs v t b -> let
-    f = free cxt t
-    u = check (sebind v t cxt) b in case f of
-      Nothing -> case u of
-        Left e -> Left $ Trace exp e
-        Right u' -> Right $ Fun t u'
-      Just f' -> Left . Trace exp $ Open t f'
-  HFunApp x y -> let
-    t = check cxt x
-    u = check cxt y in case t of
-      Left e -> Left $ Trace exp e
-      Right (Fun p r) -> case u of
-        Left e -> Left $ Trace exp e
-        Right u' | u' == p -> Right u'
-        Right u' -> Left . Trace exp $ Wrong y u' ParamArgMismatch
-      Right t' -> Left (Trace exp (Wrong x t' ExpectingFunction))
-  HIf0 x y z -> case check cxt x of
-    Left e -> Left $ Trace exp e
-    Right Nat -> case check cxt y of
-      Left e -> Left $ Trace exp e
-      Right y' -> case check cxt z of
-        Left e -> Left $ Trace exp e
-        Right z' | z' == y' -> Right z'
-        Right z' -> Left . Trace exp $ Mismatch y y' z z'
-    Right x' -> Left . Trace exp $ Unexpected x Nat x'
-  HM t x -> case free cxt t of
-    Just t' -> Left . Trace exp $ Open t t'
-    Nothing -> case check cxt x of
-      Left e -> Left $ Trace exp e
-      Right x' | x' == t -> Right x'
-      Right x' -> Left . Trace exp $ Unequal x' t
-  
-  --
-{-HNum _ -> Just Nat
+  HAdd x y -> do
+    t <- checkH cxt x
+    u <- checkH cxt y
+    assert (t == Nat && u == Nat)
+    return Nat
+  HFix x -> do
+    t <- checkH cxt x
+    case t of
+      Fun p b -> Just p
+      _ -> Nothing
+  HFunAbs v p e -> do
+    assert (closed p)
+    b <- checkH (sebind v p cxt) e
+    return (Fun p b)
+  HFunApp x y -> do
+    opr <- checkH cxt x
+    opd <- checkH cxt y
+    case opr of
+      Fun p b -> do
+        assert (p == opd)
+        return b
+      _ -> Nothing
+  HIf0 g t f -> do
+    gt <- checkH cxt g
+    tt <- checkH cxt t
+    ft <- checkH cxt f
+    assert (gt == Nat && tt == ft)
+    return tt
+  HM t e -> do
+    assert (closed t)
+    u <- checkM cxt e
+    assert (t == u)
+    return t
+  HNum _ -> Just Nat
   HSub x y -> do
     t <- checkH cxt x
     assert (t == Nat)
@@ -123,12 +74,11 @@ checkH cxt exp = case exp of
   HVar v -> sbinding v cxt
   HWrong t s -> do
     assert (closed t)
-    return t-}
+    return t
 
 -- ML
 
-checkM = undefined
-{-checkM :: Context -> MExp -> Maybe SType
+checkM :: Context -> MExp -> Maybe SType
 checkM c (MAdd x y) = do
   t <- checkM c x
   u <- checkM c y
@@ -179,7 +129,7 @@ checkM c (MTyApp e t) = do
 checkM c (MVar v) = sbinding v c
 checkM c (MWrong t s) = do
   assert (closed t)
-  return t-}
+  return t
 
 {-checkS :: [TyDef] -> Context -> SExp -> Maybe DType
 checkS _ _ = undefined-}
