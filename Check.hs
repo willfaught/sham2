96fa29dbaa5e5f30cd20e67983b105cf2d4e05a9
Will Faught <will.faught@gmail.com>
2009-11-14 16:19:55 -0800

    Check.hs: Added S cases to checkH/M. Ready to impl checkS.

--- Check.hs
+++ Check.hs
@@ -1,20 +1,17 @@
-module Check (checkH, checkM) where
+module Check (checkH, checkM, checkS) where
 
 import Context
-import Data.List (find)
-import Data.Maybe (fromJust, isJust)
 import Subst
 import Syntax
 
-closed :: SType -> Bool
-closed t = go empty t where
-  go c t = case t of
+closed :: Context -> SType -> Bool
+closed c t = case t of
     Lump -> True
     Nat -> True
     TyVar x -> sbound x c
-    Label x _ -> go c x
-    Fun x y -> go c x && go c y
-    Forall x y -> go (stbind x c) y
+    Label x _ -> closed c x
+    Fun x y -> closed c x && closed c y
+    Forall x y -> closed (stbind x c) y
 
 assert :: Bool -> Maybe Bool
 assert True = Just True
@@ -35,7 +32,7 @@ checkH cxt exp = case exp of
       Fun p b -> Just p
       _ -> Nothing
   HFunAbs v p e -> do
-    assert (closed p)
+    assert (closed cxt p)
     b <- checkH (sebind v p cxt) e
     return (Fun p b)
   HFunApp x y -> do
@@ -53,11 +50,15 @@ checkH cxt exp = case exp of
     assert (gt == Nat && tt == ft)
     return tt
   HM t e -> do
-    assert (closed t)
+    assert (closed cxt t)
     u <- checkM cxt e
     assert (t == u)
     return t
   HNum _ -> Just Nat
+  HS t e -> do
+    assert (closed cxt t)
+    checkS cxt e
+    return t
   HSub x y -> do
     t <- checkH cxt x
     assert (t == Nat)
@@ -68,68 +69,73 @@ checkH cxt exp = case exp of
     t <- checkH (stbind v cxt) e
     return (Forall v t)
   HTyApp e t -> do
-    assert (closed t)
+    assert (closed cxt t)
     Forall v u <- checkH cxt e
     return $ substTy t v u
   HVar v -> sbinding v cxt
   HWrong t s -> do
-    assert (closed t)
+    assert (closed cxt t)
     return t
 
 -- ML
 
 checkM :: Context -> MExp -> Maybe SType
-checkM c (MAdd x y) = do
-  t <- checkM c x
-  u <- checkM c y
-  assert (t == Nat && u == Nat)
-  return Nat
-checkM c (MFix x) = do
-  t <- checkM c x
-  case t of
-    Fun p b -> Just p
-    _ -> Nothing
-checkM c (MFunAbs v p e) = do
-  assert (closed p)
-  b <- checkM (sebind v p c) e
-  return (Fun p b)
-checkM c (MFunApp x y) = do
-  opr <- checkM c x
-  opd <- checkM c y
-  case opr of
-    Fun p b -> do
-      assert (p == opd)
-      return b
-    _ -> Nothing
-checkM c (MH t e) = do
-  assert (closed t)
-  u <- checkH c e
-  assert (t == u)
-  return t
-checkM c (MIf0 g t f) = do
-  gt <- checkM c g
-  tt <- checkM c t
-  ft <- checkM c f
-  assert (gt == Nat && tt == ft)
-  return tt
-checkM _ (MNum _) = Just Nat
-checkM c (MSub x y) = do
-  t <- checkM c x
-  assert (t == Nat)
-  u <- checkM c y
-  assert (u == Nat)
-  return Nat
-checkM c (MTyAbs v e) = do
-  t <- checkM (stbind v c) e
-  return (Forall v t)
-checkM c (MTyApp e t) = do
-  assert (closed t)
-  Forall v u <- checkM c e
-  return $ substTy t v u
-checkM c (MVar v) = sbinding v c
-checkM c (MWrong t s) = do
-  assert (closed t)
-  return t
+checkM cxt exp = case exp of
+  MAdd x y -> do
+    t <- checkM cxt x
+    u <- checkM cxt y
+    assert (t == Nat && u == Nat)
+    return Nat
+  MFix x -> do
+    t <- checkM cxt x
+    case t of
+      Fun p b -> Just p
+      _ -> Nothing
+  MFunAbs v p e -> do
+    assert (closed cxt p)
+    b <- checkM (sebind v p cxt) e
+    return (Fun p b)
+  MFunApp x y -> do
+    opr <- checkM cxt x
+    opd <- checkM cxt y
+    case opr of
+      Fun p b -> do
+        assert (p == opd)
+        return b
+      _ -> Nothing
+  MH t e -> do
+    assert (closed cxt t)
+    u <- checkH cxt e
+    assert (t == u)
+    return t
+  MIf0 g t f -> do
+    gt <- checkM cxt g
+    tt <- checkM cxt t
+    ft <- checkM cxt f
+    assert (gt == Nat && tt == ft)
+    return tt
+  MNum _ -> Just Nat
+  MS t e -> do
+    assert (closed cxt t)
+    checkS cxt e
+    return t
+  MSub x y -> do
+    t <- checkM cxt x
+    assert (t == Nat)
+    u <- checkM cxt y
+    assert (u == Nat)
+    return Nat
+  MTyAbs v e -> do
+    t <- checkM (stbind v cxt) e
+    return (Forall v t)
+  MTyApp e t -> do
+    assert (closed cxt t)
+    Forall v u <- checkM cxt e
+    return $ substTy t v u
+  MVar v -> sbinding v cxt
+  MWrong t s -> do
+    assert (closed cxt t)
+    return t
 
-{-checkS :: [TyDef] -> Context -> SExp -> Maybe DType
-checkS _ _ = undefined-}
+checkS :: Context -> SExp -> Maybe DType
+checkS cxt exp = undefined
