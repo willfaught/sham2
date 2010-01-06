19e0783404a4f002996094c5ce12f10442a6639e
Will Faught <will.faught@gmail.com>
2010-01-05 23:55:04 -0800

    Main.hs: Started on the program functionality.

--- Main.hs
+++ Main.hs
@@ -1,17 +1,44 @@
-module Main (evalH) where
+module Main where
 
 import Check
-import Context
-import Data.Maybe (isJust)
-import HaskellParser
+import Parse
 import Reduce
 import Syntax
+import System.Environment
+import System.FilePath.Posix
+import System.IO
+import Text.ParserCombinators.Parsec as P
 
-evalH :: String -> Maybe HExp
-evalH s = case parseH s of
-  Left x -> Nothing
-  Right x -> if isJust $ checkH empty x then Just $ reduceFullH x else Nothing
+handler ext = case lookup ext pairs of
+  Just x -> x
 
+usage :: IO ()
+usage = putStrLn "usage: sham [FILE.haskell | FILE.ml | FILE.scheme]"
 
-parseH :: String -> Either ParseError HExp
-parseH = parse mexp ""
+run :: IO ()
+run file = do
+  let handle = openFile file ReadMode
+  if hIsEOF handle
+    then hPutStrLn stderr "sham: unexpected end of file"
+    else return ()
+  if not (hasExtension file)
+    then hPutStrLn stderr "sham: file extension required to determine the language"
+    else return ()
+  let ext = takeExtension $ getArgs !! 0
+  if not (ext `elem` [".haskell", ".ml", ".scheme"])
+    then hPutStrLn stderr "sham: invalid file extension"
+  let p = parse
+  hClose stdout
+  return ()
+
+main :: IO ()
+main = do
+  if length getArgs > 1
+    then hPutStrLn stderr "sham: invalid number of arguments"
+    else if length getArgs == 0
+      then usage
+      else do
+        let arg = getArgs !! 0
+        if arg == "--help"
+          then usage
+          else run arg
